4756af19e6a4778c856c462e2a3042c75c8604fd
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-10-10 01:04:53 +0530

    Added sidebar component with style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
      <Topbar  />
+     <div className="container">
+      <Sidebar  />
+     </div>
+     <div className="other">
+     </div>
     </div>
   );
 }
