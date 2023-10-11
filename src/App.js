fd9f8459cef1d92b14e26a5466d1fbeb48e3caf6
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-10-11 22:45:35 +0530

    Added useList page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,47 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+// import NewUser from "./pages/newUser/NewUser";
+// import ProductList from "./pages/productList/ProductList";
+// import Product from "./pages/product/Product";
+// import NewProduct from "./pages/newProduct/NewProduct";
+
 
 function App() {
   return (
-    <div className="App">
-     <Topbar  />
-     <div className="container">
-      <Sidebar  />
-      <Home  />
-     </div>
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          {/* <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
+          </Route> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
