01b075313b6668e22aded0bb97fd5ab64530ea36
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-10-12 00:33:39 +0530

    Added userList,  newUser,  productList, product-edit, newProduct

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
-// import NewUser from "./pages/newUser/NewUser";
-// import ProductList from "./pages/productList/ProductList";
-// import Product from "./pages/product/Product";
-// import NewProduct from "./pages/newProduct/NewProduct";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 
 function App() {
@@ -18,27 +18,13 @@ function App() {
       <div className="container">
         <Sidebar />
         <Routes>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          {/* <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newProduct" element={<NewProduct />} />
         </Routes>
       </div>
     </Router>
